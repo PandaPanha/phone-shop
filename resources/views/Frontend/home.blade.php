2791f6a7c5de36775b50ff3af9a2f3e6f6afc4f5
XI0N9MA0 <boeursamnang11@gmail.com>
2024-05-03 00:32:28 +0700

    fixed bug

--- resources/views/Frontend/home.blade.php
+++ resources/views/Frontend/home.blade.php
@@ -37,11 +37,35 @@
     <div class="m-4">
         <h3  style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: #333; font-weight: bold; text-transform: uppercase; letter-spacing: 2px;">Smart Phone</h3>
         <hr>
-        <div class="item row row-cols-4 w-auto  justify-content-center align-content-center flex-wrap pb-5 ">
+        <div class="row gap-5 row-cols-2 row-cols-sm-3 row-cols-lg-4  justify-content-center align-content-center pb-5 ">
             @foreach ($products as $item)
+            <a href="{{ route('home.productDetail', ['id' => $item->id]) }}" class="card text-decoration-none">
+                @foreach ($product_imgs as $img)
+                    @if ($item->id == $img->product_id)
+                        <div class="d-flex justify-content-center align-content-center ">
+                            <img width="250px" height="250px" src="{{ '/assets/' . $img->product_img }}" alt="hello">
+                        </div>
+                    @endif
+                @endforeach
+                <div class="card-body text-center ">
+                    <hr>
+                    <h4 class="card-text">{{ $item->product_name }}</h4>
+                    <h5 class="card-title text-warning"><strong>$ {{ $item->price }}</strong></h5>
+                </div>
+            </a>
+            @endforeach
+        </div>
+    </div>
+    <br><br>
+</div>
+@endsection
+
+
+
+{{-- @foreach ($products as $item)
                 <a href="{{ route('home.productDetail', ['id' => $item->id]) }}" class=" text-decoration-none">
-                    <div class="card">
-                        <div class="d-flex flex-column flex-grow-0   justify-content-center align-content-center ">
+                    <div class="card d-flex flex-column align-content-center justify-content-center ">
+                        <div class="">
                             @foreach ($product_imgs as $img)
                             @if ($item->id == $img->product_id)
                                 <img src="{{ '/assets/' . $img->product_img }}" alt="" width="200px"
@@ -56,9 +80,4 @@
                         </div>
                     </div>
                 </a>
-            @endforeach
-        </div>
-    </div>
-    <br><br>
-</div>
-@endsection
+            @endforeach --}}
